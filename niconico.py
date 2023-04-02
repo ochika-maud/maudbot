48016fe8090c992897a6499b673eb1dd1a0cf2ba
ochika-maud <113527720+ochika-maud@users.noreply.github.com>
2023-04-02 20:50:00 +0900

    Add files via upload

--- niconico.py
+++ niconico.py
@@ -1,5 +1,8 @@
 import discord
 import os
+#import ffmpeg
+import aiohttp
+from discord import app_commands
 
 # インテントの生成
 intents = discord.Intents.default()
@@ -7,22 +10,40 @@ intents.message_content = True
 
 # クライアントの生成
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
 
 # discordと接続した時に呼ばれる
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+  print("起動した…")
+  await client.change_presence(activity=discord.Game(name="jubeat Ave."))
+  await tree.sync()
+
 
 # メッセージを受信した時に呼ばれる
 @client.event
 async def on_message(message):
-    # 自分のメッセージを無効
-    if message.author == client.user:
-        return
+  # 自分のメッセージを無効
+  if message.author == client.user:
+    return
 
-    # メッセージが"$hello"で始まっていたら"Hello!"と応答
-    if message.content.startswith('$ドルセ'):
-        await message.channel.send('Hello!')
+  # メッセージが"$hello"で始まっていたら"Hello!"と応答
+  if message.content.startswith('$テスト'):
+    await message.channel.send('Hello!')
+
+
+@tree.command(name="test", description="テストコマンドです。")
+async def test_command(interaction: discord.Interaction, テキスト: str):
+  if テキスト == "モード":
+    await interaction.response.send_message("どうしたの？", ephemeral=True)
+  else:
+    await interaction.response.send_message(テキスト, ephemeral=True)
+
+
+  
 
 # クライアントの実行
-client.run(os.environ.get("discordtoken"))
+my_secret = os.environ['token']
+client.run(my_secret)
+
